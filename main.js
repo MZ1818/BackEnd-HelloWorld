409292c6c7fabefd8383dcc873186b1556f2d813
MZ1818 <ahmadmufazzal18@gmail.com>
2024-06-02 11:09:38 +0530

    Episode 1

--- main.js
+++ main.js
@@ -1,86 +1,87 @@
-// Problem 2:
-// Writing Functions
-// Write a JavaScript function named calculateCircleArea that takes the radius of a circle
-// as a parameter and returns the area of the circle. You can use the formula area = π * radius^2. Test the function with a few different radii.
+// Problem 1:
+// Conditional Statements (if-else)
+// You run a movie theater, and you want to offer discounts based on a person's age. Write a JavaScript program that asks the user for their age and then displays a message:
+// - If the age is less than 18, display "You get a 20% discount!"
+// - If the age is between 18 and 65 (inclusive), display "Normal ticket price applies."
+// - If the age is 65 or older, display "You get a 30% senior discount!"
+var prompt = require("prompt-sync")();
 
-// var calculateCircleArea = function (r) {
-//   var area = Math.PI * (r * r);
-//   console.log(area);
-// };
-
-// calculateCircleArea(5);
-// calculateCircleArea(7);
-
-// Problem 3:
-//  Callback Functions
-// Create a function named performOperation that takes two numbers and a callback
-// function as parameters. The callback function should determine the operation to be performed (addition, subtraction, multiplication, or division) on the two numbers. Call the
-// performOperation function with different numbers and callback functions for each
-// mathematical operation.
-
-// function add(x, y) {
-//   return x + y;
-// }
-// function sub(x, y) {
-//   return x - y;
-// }
-// function mul(x, y) {
-//   return x * y;
-// }
-// function divide(x, y) {
-//   return x / y;
-// }
-
-// var performOperation = function (a, b, callBack) {
-//   return callBack(a, b);
-// };
-
-// console.log(performOperation(2, 3, add));
-// console.log(performOperation(2, 3, sub));
-// console.log(performOperation(2, 3, mul));
-// console.log(performOperation(2, 3, divide));
-
-// Problem 4:
-//  Using the 'fs' Module
-// Write a Node.js program that uses the fs module to read the contents of a text file named "notes.txt" and display them in the console.
-
-// var fs = require("fs");
-
-// fs.appendFile("notes.txt", "\nHello! Good Morning.", () => {
-//   console.log("file loaded");
-// });
-
-// Problem 5:
-//  Using 'os' Module
-// Create a Node.js program that uses the os module to display the following system
-// information:
-// ● Total memory available (in bytes)
-// ● Free memory available (in bytes)
-// ● Operating system platform
-// ● Number of CPU cores
-
-// var os = require("os");
-
-// var memory = os.totalmem();
-
-// console.log("memory:", memory);
-// console.log("free Memory:", os.freemem());
-// console.log("Platform:", os.platform());
-// console.log("num of CPU cores:", os.cpus().length);
-
-// Problem 6:
-//  'lodash' Usage
-// Use the lodash library to solve the following problem: Given an array of numbers, write a
-// function that returns the sum of all even numbers in the array. Use the _.sumBy function from
-// lodash to achieve this.
-
-var _ = require("lodash");
-
-var data = [2, 5, 6, 3, 8];
-
-function sumOfEven(data) {
-  var evenNumbers = _.filter(data, (num) => num % 2 === 0);
-  return _.sumBy(evenNumbers);
+var age = prompt("Enter ur age");
+if (age < 18) {
+  console.log("You get a 20% discount!");
+} else if (age >= 18 && age <= 65) {
+  console.log("Normal ticket price applies.");
+} else {
+  console.log("You get a 30% senior discount!");
 }
 
-console.log(sumOfEven(data));
+// Problem 2:
+//  Variables (var and const)
+// Create a JavaScript program to calculate the area of a rectangle. Ask the user for the length
+// and width of the rectangle and store them in variables. Calculate and display the area using
+// the formula: area = length * width.
+
+const len = prompt("enter length");
+const wid = prompt("enter width");
+const area = len * wid;
+console.log("Required Area : " + area);
+
+// Problem 3:
+//  Objects and Properties
+// You're creating an online store. Define a JavaScript object named "product" with the following properties:
+// - name (string)
+// - price (number)
+// - inStock (boolean)
+// Create at least three products using your object template and display their details using console.log.
+
+const product1 = {
+  name: "XYZ",
+  price: 55,
+  inStock: true,
+};
+const product2 = {
+  name: "abc",
+  price: 555,
+  inStock: true,
+};
+const product3 = {
+  name: "lmn",
+  price: 505,
+  inStock: true,
+};
+console.log(product1);
+console.log(product2);
+console.log(product3);
+
+// Problem 4: \
+// Arrays
+// You're organizing a party and want to keep track of the guest list. Create an array called
+// "guestList" and add the names of at least five guests. Then, write a program that checks if a
+// given name is on the guest list. If the name is found, display "Welcome to the party, [name]!"; otherwise, display "Sorry, you're not on the guest list."
+
+var guestList = ["ajay", "nini", "kiki", "sisi", "mimi"];
+var checkGuest = prompt("Enter the guest name");
+if (guestList.includes(checkGuest)) {
+  console.log("Welcome to the party," + checkGuest + "!");
+} else {
+  console.log("Sorry, you're not on the guest list.");
+}
+
+// Problem 5:
+//  JSON (JavaScript Object Notation)
+// You're working on a weather app. Create a JSON object representing the weather forecast for
+// a specific day. Include properties like "date," "temperature," "conditions," and "humidity."
+// Display the information using console.log.
+// Remember to encourage your students to experiment and think creatively while solving these problems. They can use the concepts you've taught them to come up with their own solutions.
+// This will not only help solidify their understanding but also foster their problem-solving skills in JavaScript.
+
+const weatherForecast = {
+  date: "2024-09-07",
+  temperature: 44,
+  condition: "Sunny",
+  humidity: 60,
+};
+console.log(weatherForecast.date);
+console.log(weatherForecast.temperature);
+console.log(weatherForecast.condition);
+console.log(weatherForecast.humidity);
